0706e3220369144de6b36be2e46dd5272a6200b7
KingoftheSloth <131847767+KingoftheSloth@users.noreply.github.com>
2023-09-27 12:51:41 +0900

    스프링부트 1일차

---
+++ src/main/java/org/iclass/mvc/dao/CommunityCommentsMapper.java
@@ -0,0 +1,15 @@
+package org.iclass.mvc.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.iclass.mvc.dto.CommunityComments;
+@Mapper
+public interface CommunityCommentsMapper {
+
+	int insert (CommunityComments dto);
+	int delete(long idx);
+	long maxOf();
+	List<CommunityComments> commentsList(long idx);
+	int setCommentCount(long idx);
+}
