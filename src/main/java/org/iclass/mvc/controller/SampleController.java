50af9403e969816f35f2fa6c3193bb2ab4c92015
KingoftheSloth <131847767+KingoftheSloth@users.noreply.github.com>
2023-10-04 13:00:57 +0900

    2023-10-04(오전)

--- src/main/java/org/iclass/mvc/controller/SampleController.java
+++ src/main/java/org/iclass/mvc/controller/SampleController.java
@@ -28,13 +28,13 @@ public class SampleController {
 		log.info("파라미터 name : {}" , name);
 		log.info("파라미터 age : {}" , age);
 		log.info("Community dto : {}", community); // Community 클래스의 모든 필드들이 파라미터로 전달될 수 있습니다.
+
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+
+
+
 	// required = false 로 하면 파라미터 값이 null 되어야하므로
 		// int,long 들은 Integer,Long 과 같이 래퍼(Wrapper) 타입으로 선언합니다.
-
-		model.addAttribute( "name", name);
-
 	}
-	// DTO 클래스는 파라미터를 받고 다시 view 의 Model(뷰로 전달하는 데이터 저장 객체) 로 전달도 할 수 있습니다.
-	//                  ㄴ 이 때 model 에 저장된 데이터의 이름은 dto 변수명과 같아야 합니다.
-	// int,long 등 기본형과 String은 파라미터 받은 것을 Model에 직접 저장해야 view에 전달합니다.
 }
